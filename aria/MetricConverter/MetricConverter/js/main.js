25ca371b38b95f8ae4553d09633a30010466b072
Andrew Burnett <andrew@nospam.forme>
2014-03-10 19:53:13 +0000

    Add javascript for inputs

--- aria/MetricConverter/MetricConverter/js/main.js
+++ aria/MetricConverter/MetricConverter/js/main.js
@@ -1,1 +1,40 @@
-﻿
+﻿$(document).ready(function () {
+
+    $("#convert-value").blur(function () {
+        getValues();
+        console.log("value blur");
+    });
+
+    $("#convert-from").blur(function () {
+        getValues();
+        console.log("from blur");
+    });
+
+    $("#convert-to").blur(function () {
+        getValues();
+        console.log("to blur");
+    });
+
+
+    function getValues() {
+        var to = $("#convert-to").val();
+        var from = $("#convert-from").val();
+        var value = $("#convert-value").val();
+
+        if (to === from) {
+            $("#converted-value").val(value);
+            return;
+        }
+
+        if (from.toLowerCase() === "pounds") {
+            if (to.toLowerCase() === "kilos") {
+                $("#converted-value").val(converter.poundsToKilos(value));
+            }
+        } else if (from.toLowerCase() === "kilos") {
+            if (to.toLowerCase() === "pounds") {
+                $("#converted-value").val(converter.kilosToPounds(value));
+            }
+        }
+    }
+
+});
