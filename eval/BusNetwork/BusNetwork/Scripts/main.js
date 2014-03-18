90dd990830ef844f97a4c901900152aef4af9913
Andrew Burnett <andrew@nospam.forme>
2014-03-18 21:46:58 +0000

    Split up input

--- eval/BusNetwork/BusNetwork/Scripts/main.js
+++ eval/BusNetwork/BusNetwork/Scripts/main.js
@@ -1,13 +1,17 @@
 ﻿$(document).ready(function () {
+
     $("#btn_start").on("click", function (event) {
         event.preventDefault();
-        var textin = $("#textin").text();
-        console.log(textin);
+        var textin = $("#textin").val().split("\n");
+        for (var line in textin) {
+            console.log(textin[line] + "<<")
+            var parts = textin[line].split("; ");
+            for (var p in parts) {
+                console.log("(" + parts[p] + ")");
+                // now need to make a route object
+            }
+        }
         $("#textout").html(textin);
     });
-	// what happens next
-	
-	// need to get schedule via text box
-	
-	// generate route map
+
 });
