aa57608e26f0dc7e45df773a99caf79f5940afd6
Andrew Burnett <andrew@nospam.forme>
2014-03-24 11:56:21 +0000

    Inital graph structure

--- eval/BusNetwork/SubwayNetwork/js/codeeval.graph.js
+++ eval/BusNetwork/SubwayNetwork/js/codeeval.graph.js
@@ -1,14 +1,27 @@
 ﻿codeeval.graph = (function() {
 
-    var makeVertex = function (id, edges) {
+    var makeVertex = function(id, edges) {
         var vertex = Object.create(Object);
         vertex.id = id;
         vertex.edges = edges;
         return vertex;
     };
 
+    var makeEdge = function(weight, vertex) {
+        var edge;
+
+        if (typeof vertex === 'undefined') {
+            return;
+        }
+        edge = Object.create(Object);
+        edge.weight = weight;
+        edge.vertex = vertex;
+        return edge;
+    }
+
     return {
-        makeVertex: makeVertex
+        makeVertex: makeVertex,
+        makeEdge: makeEdge
     };
 
 })();
--- eval/BusNetwork/SubwayNetwork/test/spec/GraphSpec.js
+++ eval/BusNetwork/SubwayNetwork/test/spec/GraphSpec.js
@@ -1,14 +1,30 @@
 ﻿describe('Graph data structure', function () {
 
-    var vertex;
+    var vertex, edge;
 
     beforeEach(function () {
-        var v = codeeval;
-        vertex = codeeval.graph.makeVertex(3, 2);
+        vertex = codeeval.graph.makeVertex(3, [edge]);
+        edge = codeeval.graph.makeEdge(7, vertex);
     });
 
-    it('should be able to create a vertex with id and edges', function () {
+    it('should be able to create an edge with a weight and a vertex', function () {        
+        expect(edge.weight).toBe(7);
+        expect(edge.vertex).toEqual(vertex);
+    });
+
+    it('should not be able to create an edge without a vertex', function() {
+        var e = codeeval.graph.makeEdge(4);
+        expect(e).toBeUndefined();
+    });
+
+    it('should be able to create a vertex with an id and one or more edges', function () {
+        expect(vertex.edges).toEqual([edge]);
+    });
+
+    it('should be able to create a vertex with id and no edges', function () {
+        var v = codeeval.graph.makeVertex(3);
         expect(vertex).toBeTruthy();
+        expect(v.edges).toBeUndefined();
     });
 
     it('should be able to access id value', function () {
@@ -16,3 +32,4 @@
     });
 
 });
+
