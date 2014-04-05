6d93f19901ea423a562c9794f3af38bf0ed1a87d
Andrew Burnett <andrew@nospam.forme>
2014-04-05 15:47:00 +0100

    Before DB stuff

---
+++ ent/WeekNine/SHGolfStore.Domain/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHGolfStore.Domain.Entities
+{
+    class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+
+        // Foreign Key setup
+        public int PublisherId { get; set; }
+        public virtual Publisher Publisher { get; set; }
+    }
+}
---
+++ ent/WeekNine/SHGolfStore.Domain/Entities/Publisher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHGolfStore.Domain.Entities
+{
+    class Publisher
+    {
+        // Primary Key
+        public int PublisherId { get; set; }
+
+        // Data attributes for the Publisher
+        public string PublisherName { get; set; }
+        public string Address { get; set; }
+
+        // Association - one publisher has many books
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
