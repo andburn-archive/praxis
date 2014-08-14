81af3f18858625a3a457956103a3f55c08d008e3
Andrew Burnett <andrew@nospam.forme>
2014-08-14 11:28:17 +0100

    Update sync

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
+<<<<<<< Updated upstream
 class HomeController < ApplicationController
 	before_action :authenticate_user!
 	def index
 	end
 	
 end
+=======
+class HomeController < ApplicationController
+
+	def index
+	end
+	
+end
+>>>>>>> Stashed changes
